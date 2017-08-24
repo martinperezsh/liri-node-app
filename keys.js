bd794438c83f7da7ea70650a42617314f0c60465
Martin Perez <martinperezsh@gmail.com>
2017-08-23 19:14:18 -0600

    created files and added keys

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'IvT3qDB4KtvMVDUvjaXR7Y5ko',
+  consumer_secret: 'sQVkaYZzfFGMAsVUdHn4oHVTvSS2avvlT36Fn0IqaGZmbSTdeX',
+  access_token_key: '899930878607974402-akObItzCYkFvp70mbU6AmWAbNO9xU0F',
+  access_token_secret: '9YZrlNDHVfoxF7eVULZvEp9pGwCmW2xcpQwj7S1wXVwq7',
+}
+
+// Spotify keys
+// Client ID  e078fd2c27af43678384d316b2e725a0
+// Client Secret  80ddf830d2784c2daf3c54eb0002a0d1
---
+++ liri.js
@@ -0,0 +1,21 @@
+// Linking twitter keys to this file
+var keys = require('./keys.js');
+var twitterKeys = keys.twitterKeys;
+
+
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var fs = require('fs');
+
+var input = process.argv[2];
+
+if (input === 'my-tweets') {
+
+}
+
+
+
+
+function tweets() {
+		
+	}
